764564ae601736dcaaece40a02d1ee493c0ca078
Ria Mishra <ria.mishra@intimetec.com>
2024-07-03 18:11:30 +0530

    Added client side service interface

---
+++ RecommendationSystemClient/src/main/java/services/Interfaces/IEmployeeService.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package services.Interfaces;
+
+/**
+ *
+ * @author ria.mishra
+ */
+public interface IEmployeeService {
+    public void submitFeedback();
+    public void viewRolledOutItems();
+}
